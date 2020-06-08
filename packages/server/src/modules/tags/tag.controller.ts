155699c55d3a2ffe1610440d15ddffd76dd58873
zhangjianjun301 <zhangjianjun301@pingan.com.cn>
2020-06-08 21:19:18 +0800

    nest & nuxt

--- packages/server/src/modules/tags/tag.controller.ts
+++ packages/server/src/modules/tags/tag.controller.ts
@@ -1,20 +1,20 @@
-import { Controller, Get, Post, Param, Body, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Param, Delete } from '@nestjs/common';
 import { TagService } from './tag.service';
 
 @Controller('tags')
 export class TagController {
-  constructor(private tagService: TagService) { }
+  constructor(private tagService: TagService) {}
   @Get(':text?')
   async listTags(@Param('text') text: string) {
     const { tagService } = this;
-    const tags = await (text ? tagService.find({ text }) : tagService.find())
+    const tags = await (text ? tagService.find({ text }) : tagService.find());
     return tags;
   }
 
   @Post(':text?')
   async upsertTag(@Param('text') text: string) {
     const { tagService } = this;
-    return tagService.upsert({ text }, { text })
+    return tagService.upsert({ text }, { text });
   }
 
   @Delete(':text')
