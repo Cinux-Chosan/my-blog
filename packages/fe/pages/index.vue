a174b774445e2067e92e8ec6fd1ed7a21a3391ad
zhangjianjun <179817004@qq.com>
2020-06-12 01:20:08 +0800

    nest & nuxt

--- packages/fe/pages/index.vue
+++ packages/fe/pages/index.vue
@@ -33,16 +33,18 @@ export default Vue.extend({
     // 获取首页格言
     // https://www.free-api.com/doc/372
     async fetchMotto() {
-      const type = Math.ceil(Math.random() * 45)
-      const { code, data } = await this.$axios.$get(
-        `https://v1.alapi.cn/api/mingyan?typeid=${type}`
-      )
-      if (code === 200) {
-        this.motto = `${data.content}`
-        this.author = `— ${data.author}`
-      } else {
-        this.motto = 'First, solve the problem. Then, write the code.'
-        this.author = '— John Johnson'
+      if (this.$route.name === 'index') {
+        const type = Math.ceil(Math.random() * 45)
+        const { code, data } = await this.$axios.$get(
+          `https://v1.alapi.cn/api/mingyan?typeid=${type}`
+        )
+        if (code === 200) {
+          this.motto = `${data.content}`
+          this.author = `— ${data.author}`
+        } else {
+          this.motto = 'First, solve the problem. Then, write the code.'
+          this.author = '— John Johnson'
+        }
       }
       setTimeout(this.fetchMotto, 15000)
     }
