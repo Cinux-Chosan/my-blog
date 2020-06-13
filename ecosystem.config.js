b6a967ce5495c32b8602072f95f8a84c7d755656
zhangjianjun <179817004@qq.com>
2020-06-14 01:17:19 +0800

    nest & nuxt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -35,8 +35,8 @@ module.exports = {
       ssh_options: "StrictHostKeyChecking=no",
       repo: "git@github.com:Cinux-Chosan/my-blog.git",
       path: "/var/www/my-blog",
-      "post-deploy": `\
-        && yarn
+      "post-deploy": `yarn \
+        && yarn \
         \`# 构建前端代码\` \
         && yarn fe:build\
         && echo 前端构建完成 \
