0564d6b5f1ab7e3c2069d2cf294d18ca99a8f9e6
zhangjianjun <179817004@qq.com>
2020-06-14 21:16:04 +0800

    nest & nuxt

--- packages/fe/.nuxt/vuetify/options.js
+++
@@ -1,1 +0,0 @@
-export default {"theme":{"dark":false,"themes":{"light":{"primary":"#26c6da","accent":"#424242","secondary":"#ff8f00","info":"#26a69a","warning":"#ffc107","error":"#dd2c00","success":"#00e676"}}}}
